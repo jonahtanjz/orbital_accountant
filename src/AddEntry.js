3f46059aa558394fab4a4f68b6f512b68ebdda7a
Darshan <darshan152151@hotmail.com>
2020-05-16 16:11:09 +0800

    Additions to AddEntry(Static)

--- src/AddEntry.js
+++ src/AddEntry.js
@@ -1,70 +1,161 @@
-class App extends React.Component {
+class AddEntry extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       people : ["John","Tube","P","Cheow"],
-      display : [],
+      displayPay : [],
+      displayConsume : [],
     }
-    this.onChange = this.onChange.bind(this);
-    this.remove = this.remove.bind(this);
+    this.onChangePay = this.onChangePay.bind(this);
+    this.onChangeConsume = this.onChangeConsume.bind(this);
+    this.removePay = this.removePay.bind(this);
+    this.removeConsume = this.removeConsume.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
   }
 
-  onChange(e) {
+  onChangePay(e) {
     const theName = e.target.value;
-    var display = this.state.display.slice();
-    display.push(theName);
-    console.log(display);
+    var display = this.state.displayPay.slice();
+    if (! display.includes(theName)) {
+      display.push(theName);
+      this.setState({
+        displayPay : display,
+      });
+    }
+  }
+  onChangeConsume(e) {
+    const theName = e.target.value;
+    var display = this.state.displayConsume.slice();
+    if (! display.includes(theName)) {
+      display.push(theName);
+      this.setState({
+        displayConsume : display,
+      });
+    }
+  }
+
+  removePay(theName) {
+    var display = this.state.displayPay.slice();
+    const index = display.findIndex((x) => x === theName);
+    display.splice(index,1);
     this.setState({
-      display : display,
+      displayPay : display,
     });
   }
 
-  remove(theName) {
-    var display = this.state.display.slice();
-    const index = display.findIndex((x)=> {return x===theName});
-    display.splice(index - 1 ,1);
-    console.log(display);
+  removeConsume(theName) {
+    var display = this.state.displayConsume.slice();
+    const index = display.findIndex((x) => x === theName);
+    display.splice(index,1);
     this.setState({
-      display : display,
+      displayConsume : display,
     });
   }
 
   onSubmit(e) {
-    console.log(e.target);
-    //alert(e.target.P.value);
-      }
+    alert(e.target.Ppay.value);
+  }
 
-    render() {
-      const list = this.state.people.map((person) => {
-        return (<option value = {person}> {person} </option>);
-      } );
-      const listAmounts = this.state.display.map((person) => {
-        return (
-          <div>
-          <lable>{person}</lable>
-          <input
-          type = "number"
-          name = {person} 
-          id = {person} 
-          placeholder = "Input Amount"
-          />
-          <input type = "button" onClick ={ () => {this.remove({person})}} value = "Delete"/>
-          </div>
-        );
-      })
+  render() {
+    const submitButton =  (<input type = "submit" value = "Submit"/> );
+    return (
+      <div>
+      <div>
+      <form onSubmit = {this.onSubmit}>
+      <p>People who paid:</p>
+        <NameList 
+        people = {this.state.people}
+        onChange = {this.onChangePay}
+        />
+        <AmountDisplay
+        onSubmit = {this.onSubmit}
+        remove = {this.removePay}
+        display = {this.state.displayPay}
+        type = "pay"
+        />
+        <p>People who consumed:</p>
+        <NameList 
+        people = {this.state.people}
+        onChange = {this.onChangeConsume}
+        type = "consume"
+        />
+        <AmountDisplay
+        onSubmit = {this.onSubmit}
+        remove = {this.removeConsume}
+        display = {this.state.displayConsume}
+        />
+        
+        {submitButton}
+      </form>
+      </div>
+      </div>
+    );
+  }
+}
+
+class AmountDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+
+  onSubmit(e) {
+    this.props.onSubmit(e);
+  }
+
+  remove(e) {
+    this.props.remove(e);
+  }
+
+  render() {
+    const listAmounts = this.props.display.map((person) => {
       return (
-        <div>
-        <form onSubmit = {this.onSubmit}>
-          <select multiple = {true}
-            onChange = {this.onChange}>
-            {list}
-          </select>
-          {listAmounts}
-          <input type = "submit" value = "Submit"/>
-        </form>
+        <div key = {person}>
+        <p>{person}</p>
+        <input
+        type = "number"
+        name = {person + this.props.type} 
+        id = {person + this.props.type} 
+        placeholder = "Input Amount"
+        />
+        <input type = "button" onClick ={ () => {this.remove(person)}} value = "Delete"/>
         </div>
       );
-    }
+    })
+    return (
+      <div>
+        {listAmounts}
+      </div>
+    );
+  }
 }
+
+class NameList extends React.Component {
+  constructor(props){
+    super(props);
+    this.onChange = this.onChange.bind(this);
+
+  }
+
+  onChange(e) {
+    this.props.onChange(e);
+  }
+
+  render() {
+    const list = this.props.people.map((person) => {
+      return (<option key = {person} value = {person}> {person} </option>);
+    } );
+    return (
+      <div>
+        <select multiple = {true}
+          onChange = {this.onChange}>
+          {list}
+        </select>
+      </div>
+    );
+  }
+}
+
+export default AddEntry;
