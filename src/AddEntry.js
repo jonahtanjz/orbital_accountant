f271c423e07e610321d962cabbcc1ddc0a5e00c6
Darshan <darshan152151@hotmail.com>
2020-05-18 17:59:45 +0800

    Added import statements

--- src/AddEntry.js
+++ src/AddEntry.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
+
 class AddEntry extends React.Component {
   constructor(props){
     super(props);
