d136bff7aa2659ab80fe9ac2f3cac3b3535d1daf
Darshan <darshan152151@hotmail.com>
2020-06-11 14:15:07 +0800

    Fixed wrongly displayed pay message in ViewLedger

--- src/Components/ViewLedger.js
+++ src/Components/ViewLedger.js
@@ -190,9 +190,9 @@ class DisplayTable extends React.Component {
         });
         let desc;
         if (total > 0) {
-            desc = " To Pay"
-        } else {
             desc = " To Receive"
+        } else {
+            desc = " To Pay"
         }
         return(
             <ExpansionPanel>
@@ -236,68 +236,6 @@ class DisplayTable extends React.Component {
                 </ExpansionPanelDetails>
             </ExpansionPanel>
 
-        );
-    }
-}
-
-class SuggestPay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            total : {}
-        }
-    }
-
-    render() {
-        let total = {}
-        this.props.users.map((user) => {
-            total[user.name] = 0
-            return user;
-        });
-        this.props.transactions.map((entry) =>{
-            let conversion;
-            let usedCurrency = this.props.currency.filter((curr) => curr.name === entry.currency);
-            if (usedCurrency.length === 0) {
-                conversion = 1;
-            } else {
-                conversion = usedCurrency[0].value;
-            }
-            let value = parseFloat((entry.amount/conversion).toFixed(2));
-            total[entry.payee] = total[entry.payee] - value;
-            total[entry.payer] += value;
-            return entry;
-        });
-        const arr = Object.keys(total).map((key) => [key, total[key]]);
-        let arr2 =arr.sort((c,d) => {
-            let a = c[1];
-            let b = d[1];
-            if (a > 0 && b > 0) {
-                return a  - b;
-            } else if (a < 0 && b < 0) {
-                return -b + a;
-            } else {
-                return -a + b;
-            }
-        })
-        let amt = 0;
-        let display = arr2.filter((total) => total[1].toFixed(2) !=0 ).map((total) =>{
-            amt += total[1]
-            
-                if (amt.toFixed(2) != 0) {
-                    return(
-                    <span>{total[0]+ " Pays $" + amt.toFixed(2) + " -> "}</span>
-                    );
-                } else {
-                    return(
-                    <span>{total[0]}</span>
-                    );
-                }
-
-        });
-        return(
-            <div>
-                {display}
-            </div>
         );
     }
 }
